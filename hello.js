9453d6dfed368328631c158ca9f0a3f193553bb9
sjini <s.jishkariani@gmail.com>
2016-01-30 21:57:02 +0300

    fgh
    fghgfh

--- hello.js
+++ hello.js
@@ -15,7 +15,17 @@ var label = tabris.create("TextView", {
 
 button.on("select", function() {
 	//page2.open();
+<<<<<<< HEAD
 	GPSLocation.getCurrentPosition(onSuccess, onError);
+=======
+	window.plugins.GPSLocator.getLocation(function(result){
+    	//alert(JSON.stringify(result));//result[0]:latitude,result[1]:longitude.
+		label.set("text", JSON.stringify(result));
+    },function(e){
+        //alert(JSON.stringify(e));//Error Message
+		label.set("text", JSON.stringify(e));
+    });
+>>>>>>> parent of c4e73d5... sas
     //label.set("text", "Totally Rock!");
 });
 
