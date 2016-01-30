520f9721a6e92b7a57687ba70ceb9266c0957a31
sjini <s.jishkariani@gmail.com>
2016-01-30 20:12:42 +0300

    asas
    asa

--- hello.js
+++ hello.js
@@ -14,7 +14,29 @@ var label = tabris.create("TextView", {
 }).appendTo(page);
 
 button.on("select", function() {
-  label.set("text", "Totally Rock!");
+	page2.open();
+    //label.set("text", "Totally Rock!");
 });
 
 page.open();
+
+var page2 = tabris.create("Page", {
+  title: "Hello, World! 222",
+  topLevel: true
+});
+
+var button2 = tabris.create("Button", {
+  text: "Native Widgets 222",
+  layoutData: {centerX: 0, top: 100}
+}).appendTo(page2);
+
+var label2 = tabris.create("TextView", {
+  font: "24px",
+  layoutData: {centerX: 0, top: [button, 50]}
+}).appendTo(page2);
+
+button2.on("select", function() {
+  label2.set("text", "Totally Rock! 222");
+});
+
+
