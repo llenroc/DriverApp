e9c6f823bf1f84570fb85c2864d5562686d5798b
sergi_jini <s.jishkariani@gmail.com>
2016-01-31 17:10:18 +0300

    settings

--- driverapp.js
+++ driverapp.js
@@ -14,11 +14,12 @@ var mainPage = tabris.create("Page", {
     topLevel: true
 });
 
+
 tabris.create("Action", {
   title: "Settings",
   image: {src: "res/images/settings.png", scale: scaleFactor}
 }).on("select", function() {
-  openSettings();
+
 });
 
 
@@ -35,7 +36,6 @@ var button_1 = tabris.create("Button", {
   text: "Some action",
   layoutData: {centerX: 0, top: [button, 5]}
 }).appendTo(mainPage);
-
 
 
 
@@ -61,6 +61,58 @@ var label2 = tabris.create("TextView", {
 }).appendTo(mainPage);
 
 
+var settingsPage = tabris.create("Page", {
+  title: "Configuretion",
+    background: "blue",
+  topLevel: true
+}); 
+
+tabris.create("TextView", {
+  id: "carIDLabel",
+  alignment: "left",
+  text: "Enter Car ID:"
+}).appendTo(settingsPage);
+
+tabris.create("TextInput", {
+  id: "CarID",
+  message: "
+}).appendTo(settingsPage);
+
+tabris.create("TextView", {
+  id: "pass",
+  text: "Enter Password:"
+}).appendTo(settingsPage);
+
+tabris.create("TextInput", {
+  id: "passInput",
+  type: "password",
+  message: ""
+}).appendTo(settingsPage);
+
+
+tabris.create("Button", {
+  id: "done",
+  text: "Change Car ID",
+  background: "#8b0000",
+  textColor: "white"
+}).on("select", function() {
+  populateMessage();
+}).appendTo(settingsPage);
+
+
+settingsPage.apply({
+  "#carIDLabel": {layoutData: {left: 10, top: 18, width: 120}},
+  "#CarID": {layoutData: {left: "#carIDLabel 10", right: 10, baseline: "#carIDLabel"}},
+  "#pass": {layoutData: {left: 10, top: "#CarID 18", width: 120}},
+  "#passInput": {layoutData: {left: "#pass 10", right: 10, baseline: "#pass"}},
+  "#done": {layoutData: {left: 10, right: 10, top: "#passInput 18"}}
+});
+
+
+function populateMessage() {
+
+}
+ 
 
 // End of object declarations ///////////////////////////////////////
 
@@ -74,24 +126,11 @@ button.on("select", function() {
 // End of event binding /////////////////////////////////////////////
 
 
-setInterval(function(){ myTimer+=1; label2.set("text", myTimer.toString()) }, 1000);
+setInterval(function(){ 
+    myTimer+=1; label2.set("text", myTimer.toString())
+    GPSLocation.getCurrentPosition(onSuccess, onError);
+}, 1000);
 
-
-function openSettings(){
-    
-    createSettingsPage().open();
-    
-}
-
-
-
-function createSettingsPage() {
-var settingsPage = tabris.create("Page", {
-  title: "Hello, World! 222",
-  topLevel: true
-}); 
-  return settingsPage;
-}
 
 
 // onSuccess Callback
