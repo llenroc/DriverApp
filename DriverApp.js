56230c997e834399042e2aa8229a70af0820646c
sergi_jini <s.jishkariani@gmail.com>
2016-01-31 12:14:04 +0300

    111

--- DriverApp.js
+++ DriverApp.js
@@ -1,6 +1,7 @@
 var page = tabris.create("Page", {
-  title: "Driver App",
-  topLevel: true
+    title: "Driver App",
+    //image: "images/my-page.png",
+    topLevel: true
 });
 
 var button = tabris.create("Button", {
@@ -24,7 +25,6 @@ var button_3 = tabris.create("Button", {
 }).appendTo(page);
 
 
-
 var label = tabris.create("TextView", {
   font: "12px",
   layoutData: {centerX: 0, top: [button, 50]}
@@ -36,7 +36,11 @@ button.on("select", function() {
     //label.set("text", "Totally Rock!");
 });
 
-var ttt = 0;
+widget.on("swipe:left", function(widget, event) {
+  page2.open();
+});
+
+//var ttt = 0;
 
 //setInterval(function(){ ttt+=1; label.set("text", ttt.toString()) }, 1000);
 
@@ -45,20 +49,6 @@ page.open();
 var page2 = tabris.create("Page", {
   title: "Hello, World! 222",
   topLevel: true
-});
-
-var button2 = tabris.create("Button", {
-  text: "Native Widgets 222",
-  layoutData: {centerX: 0, top: 100}
-}).appendTo(page2);
-
-var label2 = tabris.create("TextView", {
-  font: "24px",
-  layoutData: {centerX: 0, top: [button, 50]}
-}).appendTo(page2);
-
-button2.on("select", function() {
-  label2.set("text", "Totally Rock! 222");
 });
 
 
